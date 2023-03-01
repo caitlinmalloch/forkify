55140b955079971903ac432cdba534ba9ffcf1af
Caitlin Malloch <caitlin.malloch@gmail.com>
2023-03-01 15:51:16 -0800

    DOM updating algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,7 +16,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     // 0. Update results view to mark selected search result
-    // resultsView.update(model.getSearchResultsPage());
+    resultsView.update(model.getSearchResultsPage());
 
     // 1. Loading recipe
     await model.loadRecipe(id);
@@ -62,7 +62,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
